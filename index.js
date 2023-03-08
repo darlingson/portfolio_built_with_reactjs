4949f0906ad76a959b4e37b71b28ee72ecabd58d
Darlingson Makuwila <62630338+darlingson@users.noreply.github.com>
2023-03-08 18:40:35 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+    return /*#__PURE__*/(
+      React.createElement("div", { className: "container" }, /*#__PURE__*/
+      React.createElement(ShortIntro, null), " ", /*#__PURE__*/React.createElement(Education, null), " ", /*#__PURE__*/React.createElement(TechnicalSkills, null), " ", /*#__PURE__*/React.createElement(SoftSkills, null)));
+
+
+  }}
+
+function ShortIntro() {
+  return /*#__PURE__*/(
+    React.createElement("div", { className: "row" }, /*#__PURE__*/
+    React.createElement("div", { className: "row" }, /*#__PURE__*/
+    React.createElement("div", { className: "col s4" }, "Gender : "), /*#__PURE__*/
+    React.createElement("div", { className: "col s6" }, "Male")), /*#__PURE__*/
+
+    React.createElement("div", { className: "row" }, /*#__PURE__*/
+    React.createElement("div", { className: "col s4" }, " Languages :"), /*#__PURE__*/
+    React.createElement("div", { className: "col s4" }, " Chichewa, English")), /*#__PURE__*/
+
+    React.createElement("div", { className: "row" }, /*#__PURE__*/
+    React.createElement("div", { className: "col s4" }, "Location : "), /*#__PURE__*/
+    React.createElement("div", { className: "col s6" }, "Blantyre, Malawi"))));
+
+
+
+}
+function Education() {
+  return /*#__PURE__*/(
+    React.createElement("div", null, /*#__PURE__*/
+    React.createElement(Qualification, {
+      name: "BSc Management Information Systems",
+      school: "Malawi University of Business and Applied Sciences",
+      from: "03/2018",
