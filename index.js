e2cea8ce14f48eee712da53b86f70152fb97b9fa
Darlingson Makuwila <62630338+darlingson@users.noreply.github.com>
2023-04-09 18:43:13 +0200

    Added projects

--- index.js
+++ index.js
@@ -1,6 +1,92 @@
+class App extends React.Component {
+  render() {
+    return /*#__PURE__*/(
+      React.createElement("div", null,
+      " ", /*#__PURE__*/
+      React.createElement(WelcomeBanner, null), /*#__PURE__*/
+      React.createElement(AboutMeSection, null), /*#__PURE__*/
+      React.createElement(Projects, null), /*#__PURE__*/
+      React.createElement(Contact, null)));
+
+
+  }}
+
+class Projects extends React.Component {
+  render() {
+    return /*#__PURE__*/(
+      React.createElement(React.Fragment, null, /*#__PURE__*/
+      React.createElement("h1", null, "Projects"), /*#__PURE__*/
+      React.createElement(Project, {
+        name: "Text to sign language translator",
+        url: "",
+        desc: "A system built using PHP. It allows users to enter text or voice input and get Malawi sign language of the input in form of  short gifs.  The purpose of the system to fill shortfall of Malawi Sign Language Translators." }), /*#__PURE__*/
+
+      React.createElement(Project, {
+        name: "Personal portfolio",
+        desc: "built 2 personpersonal portfolio. The 1 was built with just css,js and . The second one was react learning project" }), /*#__PURE__*/
+
+      React.createElement(Project, {
+        name: "MUBAS Library past papers repository ",
+        desc: "Final year project built using django. The system allows for storing of digital past papers.It also allows students to access these past papers through a web web portal." })));
+
+
+
+  }}
+
+class WelcomeBanner extends React.Component {
+  render() {
+    return /*#__PURE__*/React.createElement(BannerContent, null);
+  }}
+
+class Project extends React.Component {
+  render() {
+    return /*#__PURE__*/(
+      React.createElement("div", { class: "row" }, /*#__PURE__*/
+      React.createElement("div", { class: "col s12" }, /*#__PURE__*/
+      React.createElement("div", { class: "card blue-grey darken-1 cardwidth" }, /*#__PURE__*/
+      React.createElement("div", { class: "card-content white-text" }, /*#__PURE__*/
+      React.createElement("span", { class: "card-title" }, this.props.name), /*#__PURE__*/
+
+      React.createElement("p", null, this.props.desc)), /*#__PURE__*/
+
+
+      React.createElement("div", { class: "card-action" }, /*#__PURE__*/
+      React.createElement("a", { href: "#" }, "View Demo"))))));
+
+
+
+
+
+  }}
+
+class BannerContent extends React.Component {
+  render() {
+    return /*#__PURE__*/(
+      React.createElement("div", null, /*#__PURE__*/
+      React.createElement(WelcomeMessage, null), /*#__PURE__*/
+      React.createElement(Link, { text: "about me", url: "contact.html" }), /*#__PURE__*/
+      React.createElement(Link, { text: "contact me ", url: "contact.html" }), /*#__PURE__*/
+      React.createElement(Link, { text: "my projects ", url: "contact.html" }), /*#__PURE__*/
+      React.createElement(Link, { text: "my resume ", url: "contact.html" })));
+
+
+  }}
+
+class AboutMeSection extends React.Component {
+  render() {
     return /*#__PURE__*/(
       React.createElement("div", { className: "container" }, /*#__PURE__*/
       React.createElement(ShortIntro, null), " ", /*#__PURE__*/React.createElement(Education, null), " ", /*#__PURE__*/React.createElement(TechnicalSkills, null), " ", /*#__PURE__*/React.createElement(SoftSkills, null)));
+
+
+  }}
+
+class Contact extends React.Component {
+  render() {
+    return /*#__PURE__*/(
+      React.createElement(React.Fragment, null, /*#__PURE__*/
+      React.createElement("h1", null, "Contact"), /*#__PURE__*/
+      React.createElement("a", { href: "github.com" }, "Github")));
 
 
   }}
@@ -25,8 +111,127 @@ function ShortIntro() {
 }
 function Education() {
   return /*#__PURE__*/(
+    React.createElement(React.Fragment, null, /*#__PURE__*/
+    React.createElement("h1", null, "Education "), /*#__PURE__*/
     React.createElement("div", null, /*#__PURE__*/
     React.createElement(Qualification, {
       name: "BSc Management Information Systems",
       school: "Malawi University of Business and Applied Sciences",
       from: "03/2018",
+      to: "12/2022" }), /*#__PURE__*/
+
+    React.createElement(Qualification, {
+      name: "Malawi School Certificate of Education",
+      school: "Chila PVT Secondary School",
+      from: "2017",
+      to: "2017" }))));
+
+
+
+
+}
+function Qualification(props) {
+  return /*#__PURE__*/(
+    React.createElement("div", { className: "row" }, /*#__PURE__*/
+    React.createElement("div", null, props.name), /*#__PURE__*/
+    React.createElement("div", null, props.school), /*#__PURE__*/
+    React.createElement("div", null,
+    props.from, " - ", props.to)));
+
+
+
+}
+function TechnicalSkills() {
+  return /*#__PURE__*/(
+    React.createElement(React.Fragment, null, /*#__PURE__*/
+    React.createElement("h2", null, "Technical Skills"), /*#__PURE__*/
+    React.createElement("div", null, /*#__PURE__*/
+    React.createElement("h3", null, "Frontend Web development"), /*#__PURE__*/
+    React.createElement(TechList, { tlist: frontendSkills })), /*#__PURE__*/
+
+    React.createElement("div", null, /*#__PURE__*/
+    React.createElement("h3", null, "Backend Web development"), /*#__PURE__*/
+    React.createElement(TechList, { tlist: backendSkills })), /*#__PURE__*/
+
+
+    React.createElement("div", null, /*#__PURE__*/
+    React.createElement("h3", null, "Software development"), /*#__PURE__*/
+    React.createElement(TechList, { tlist: softwareSkills })), /*#__PURE__*/
+
+
+    React.createElement("div", null, /*#__PURE__*/
+    React.createElement("h3", null, "Data Science"), /*#__PURE__*/
+    React.createElement(TechList, { tlist: dsSkills }))));
+
+
+
+}
+function SoftSkills() {
+  return /*#__PURE__*/(
+    React.createElement(React.Fragment, null, /*#__PURE__*/
+    React.createElement("div", null, /*#__PURE__*/
+    React.createElement("h2", null, "Softskills"), /*#__PURE__*/
+    React.createElement(TechList, { tlist: softSkills }))));
+
+
+
+}
+function WelcomeMessage() {
+  return /*#__PURE__*/(
+    React.createElement("div", null, /*#__PURE__*/
+    React.createElement("h1", null, /*#__PURE__*/
+    React.createElement("span", null, " Hi, I am Darlingson ")), /*#__PURE__*/
+
+    React.createElement("h2", null, /*#__PURE__*/
+    React.createElement("span", null, " Full Stack Developer and Data Scientist"))));
+
+
+
+}
+function Link(props) {
+  return /*#__PURE__*/(
+    React.createElement("button", {
+      className: "waves-effect waves-light btn  btn-small",
+      href: props.url },
+
+    props.text, " "));
+
+
+}
+function TechList(props) {
+  const list = props.tlist;
+
+  const listItems = list.map(item => /*#__PURE__*/React.createElement("li", null, item));
+
+  return /*#__PURE__*/React.createElement("ul", null, listItems);
+}
+const softSkills = ["Team worker", "independent", "fast learner"];
+const frontendSkills = [
+"react ",
+"bootstrap",
+"JQUERY",
+"HTML",
+"CSS",
+"JavaScript"];
+
+const backendSkills = [
+"django",
+"Laravel",
+"php",
+"nodejs",
+"MySQL",
+"firebase"];
+
+const softwareSkills = ["C++", "java", "python", "kotlin", "dart", "flutter"];
+const dsSkills = [
+"pandas",
+"MS Excel",
+"Google Sheets",
+"google data studio",
+"CSS",
+"JavaScript"];
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+let rootElement = /*#__PURE__*/React.createElement(App, null);
+root.render(rootElement);
+
